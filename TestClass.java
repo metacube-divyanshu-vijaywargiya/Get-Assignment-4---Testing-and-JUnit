6d252c5d28f6b66d8ea7a4636171c52e33487fc6
metacube-divyanshu-vijaywargiya <divyanshu.vijaywargiya@metacube.com>
2024-10-24 14:03:46 +0530

    Update TestClass.java

--- TestClass.java
+++ TestClass.java
@@ -10,53 +10,50 @@ public class TestClass {
 	//Test for max mirror 
 	@Test
 	public void maxMirrorTest() {
-		
-        Object[][] testCases = {
-                {new int[]{1, 2, 3, 8, 9, 3, 2, 1}, 3}, // Expected mirror sequence: [3, 2, 1]
-                {new int[]{1, 2, 3, 2, 1}, 5},          // Expected mirror sequence: [1, 2, 3, 2, 1]
-                {new int[]{1, 2, 3, 4, 5}, 1},          // Expected mirror sequence: [1] or [2] or [3] or [4] or [5]
-                {new int[]{5, 6, 7, 8, 9, 7, 6, 5}, 3}, // Expected mirror sequence: [5, 6, 7]
-                {new int[]{1, 2, 3, 4, 5, 6, 7, 8}, 1}  // Expected mirror sequence: [1] or [2] or ... [8]
-            };
-
-        // iterate through each test case in testCases
-        for (Object[] testCase : testCases) {
-            int[] inputArray = (int[]) testCase[0];		
-            int expectedOutput = (int) testCase[1];
-            int actualOutput = arrOperation.maxMirror(inputArray);
-            assertEquals(expectedOutput, actualOutput);
-        }
+	        Object[][] testCases = {
+	                {new int[]{1, 2, 3, 8, 9, 3, 2, 1}, 3}, // Expected mirror sequence: [3, 2, 1]
+	                {new int[]{1, 2, 3, 2, 1}, 5},          // Expected mirror sequence: [1, 2, 3, 2, 1]
+	                {new int[]{1, 2, 3, 4, 5}, 1},          // Expected mirror sequence: [1] or [2] or [3] or [4] or [5]
+	                {new int[]{5, 6, 7, 8, 9, 7, 6, 5}, 3}, // Expected mirror sequence: [5, 6, 7]
+	                {new int[]{1, 2, 3, 4, 5, 6, 7, 8}, 1}  // Expected mirror sequence: [1] or [2] or ... [8]
+	            };
+	
+	        // iterate through each test case in testCases
+	        for (Object[] testCase : testCases) {
+	            int[] inputArray = (int[]) testCase[0];		
+	            int expectedOutput = (int) testCase[1];
+	            int actualOutput = arrOperation.maxMirror(inputArray);
+	            assertEquals(expectedOutput, actualOutput);
+	        }
 	}
 	
 	
 	//Test for clumps count
 	@Test
-	public void countClumpsTest() {
-		
-        Object[][] testCases = {
-                {new int[]{1, 2, 2, 3, 4, 4}, 2},		// Expected clumps count: 2
-                {new int[]{1, 1, 2, 1, 1}, 2},			// Expected clumps count: 2
-                {new int[]{1, 1, 1, 1, 1}, 1},     		// Expected clumps count: 1
-            };
-        
-        // iterate through each test case in testCases
-        for (Object[] testCase : testCases) {
-            int[] inputArray = (int[]) testCase[0];
-            int expectedOutput = (int) testCase[1];
-            int actualOutput = arrOperation.countClumps(inputArray);
-            assertEquals(expectedOutput, actualOutput);
-        }
+	public void countClumpsTest() {	
+	        Object[][] testCases = {
+	                {new int[]{1, 2, 2, 3, 4, 4}, 2},		// Expected clumps count: 2
+	                {new int[]{1, 1, 2, 1, 1}, 2},			// Expected clumps count: 2
+	                {new int[]{1, 1, 1, 1, 1}, 1},     		// Expected clumps count: 1
+	            };
+	        
+	        // iterate through each test case in testCases
+	        for (Object[] testCase : testCases) {
+	            int[] inputArray = (int[]) testCase[0];
+	            int expectedOutput = (int) testCase[1];
+	            int actualOutput = arrOperation.countClumps(inputArray);
+	            assertEquals(expectedOutput, actualOutput);
+	        }
 	}
 	
 	
 	//Test for splitArray
 	@Test
-	public void splitArrayTest() {
-		
+	public void splitArrayTest() {	
 	     Object[][] testCases = {
 	                {new int[]{1, 1, 1, 2, 1}, 3},			// Expected split array index: 3
 	                {new int[]{2, 1, 1, 2, 1}, -1},			// Expected split array index: -1
-	                {new int[]{10, 10}, 1},					// Expected split array index: 1
+	                {new int[]{10, 10}, 1},				// Expected split array index: 1
 	           };
 	     
 	     // iterate through each test case in testCases
@@ -73,11 +70,11 @@ public class TestClass {
 	@Test
 	public void fixXYTest() {
 		
-        Object[][] testCases = {
-                {new int[]{5, 4, 9, 4, 9, 5}, new int[]{9, 4, 5, 4, 5, 9}},				// Expected rearranged array: {9, 4, 5, 4, 5, 9}
-                {new int[]{1, 4, 1, 5}, new int[]{1, 4, 5, 1}},							// Expected rearranged array: {1, 4, 5, 1}
-                {new int[]{1, 4, 1, 5, 5, 4, 1}, new int[]{1, 4, 5, 1, 1, 4, 5}},     	// Expected rearranged array: {1, 4, 5, 1, 1, 4, 5}
-            };
+	        Object[][] testCases = {
+	                {new int[]{5, 4, 9, 4, 9, 5}, new int[]{9, 4, 5, 4, 5, 9}},		// Expected rearranged array: {9, 4, 5, 4, 5, 9}
+	                {new int[]{1, 4, 1, 5}, new int[]{1, 4, 5, 1}},				// Expected rearranged array: {1, 4, 5, 1}
+	                {new int[]{1, 4, 1, 5, 5, 4, 1}, new int[]{1, 4, 5, 1, 1, 4, 5}},     	// Expected rearranged array: {1, 4, 5, 1, 1, 4, 5}
+		};
         
 	     // iterate through each test case in testCases
 	     for (Object[] testCase : testCases) {
